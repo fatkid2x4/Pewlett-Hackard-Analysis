02255c52a5aa7f04b60a1826b4c9b49bd32272bb
fatkid2x4 <117381236+fatkid2x4@users.noreply.github.com>
2023-01-17 17:06:31 -0600

    Add files via upload

--- PH-EmployeeDB.sql
+++ PH-EmployeeDB.sql
@@ -6,18 +6,20 @@ CREATE TABLE departments (
      UNIQUE (dept_name)
 );
 
--- Creating 2nd table
-CREATE TABLE Employees (
+SELECT * FROM departments;
+
+CREATE TABLE employees (
      emp_no INT NOT NULL,
      birth_date DATE NOT NULL, 
 	 first_name VARCHAR NOT NULL,
 	 last_name VARCHAR NOT NULL,
 	 gender VARCHAR NOT NULL,
 	 hire_date DATE NOT NULL,
-     PRIMARY KEY (emp_no),
+     PRIMARY KEY (emp_no)
 );
 
--- Creating 3rd table
+SELECT * FROM employees;
+
 CREATE TABLE dept_manager (
 dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
@@ -27,8 +29,8 @@ FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
+SELECT * FROM dept_manager;
 
--- Creating 4th table
 CREATE TABLE salaries (
   emp_no INT NOT NULL,
   salary INT NOT NULL,
@@ -38,4 +40,24 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
-SELECT * FROM departments;.
+SELECT * FROM salaries;
+
+CREATE TABLE titles (
+  emp_no INT NOT NULL,
+  Title VARCHAR NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+SELECT * FROM titles;
+
+CREATE TABLE dept_emp (
+  emp_no INT NOT NULL,
+  dept_no INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+SELECT * FROM dept_emp;
